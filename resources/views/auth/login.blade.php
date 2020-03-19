e3bd4f5f23063b4f82e6b8a4b06207d847acdfb5
Błażej <darullef4@gmail.com>
2020-03-19 21:49:10 +0100

    L-5 Utworzenie widoku rejestrowania nowego użytkownika.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,21 +5,30 @@
    <div class="row center">
        <div class="col m12">
            <div class="card">
-               <h4>Login</h4>
+               <h4>{{ __('Login') }}</h4>
                <div class="card-content">
                    <form method="POST" action="{{ route('login') }}">
                        @csrf
                        <div class="row">
                            <div class="input-field">
-                               <input id="email" type="email" class="validate" name="email" required autocomplete="email" autofocus>
-                               <label for="email">Email</label>
-                               <span class="helper-text" data-error="Wrong email format" data-success=""> </span>
+                               <input id="email" type="email" class="validate @error('email') invalid @enderror" name="email" required autocomplete="email" autofocus>
+                               <label for="email">{{ __('E-Mail Address') }}</label>
                            </div>
                        </div>
                        <div class="row">
                            <div class="input-field">
-                               <input id="password" type="password" class="validate" name="password" required autocomplete="current-password">
-                               <label for="password">Password</label>
+                               <input id="password" type="password" class="validate @error('password') invalid @enderror" name="password" required autocomplete="current-password">
+                               <label for="password">{{ __('Password') }}</label>
+
+                               @if ($errors->any())
+                               <span class="red-text" role="alert">
+                                   @foreach ($errors->all() as $error)
+                                       {{ $error }}
+                                       <br/>
+                                   @endforeach
+                               </span>
+                               @endif
+
                            </div>
                        </div>
                        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
