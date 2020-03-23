753c8688cfa706f4d466cb0fb1d6400a84d530f3
Bartek Olszanowski <baloo@int.pl>
2020-03-23 22:50:57 +0100

    L-37 Zmiana na public_html

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix
+    .setPublicPath("public_html/")
     .sourceMaps(false, "source-map")
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+    .js('resources/js/app.js', 'js')
+    .sass('resources/sass/app.scss', 'css')
     .disableSuccessNotifications();
 
