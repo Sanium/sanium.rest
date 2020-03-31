02408a85de2c5af2a6cd6dcbc4fa61e10f74f83d
Błażej <darullef4@gmail.com>
2020-03-31 18:04:57 +0200

    L-52 Reformat widoku dashboardu.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,82 +6,75 @@
 
 @section('content')
     <div class="container">
-        <div class="card" style="margin-bottom: 2rem">
-            <div class="row card-content valign-wrapper">
-                <div class="col">
-                    <img name="company-logo" class="responsive-img circle" width="60"
-                         src="{{ $employer->getLogo() }}">
+        <div class="card mt-5">
+            <div class="card-body row d-flex align-items-center">
+                <div class="col-12 col-sm-2 d-flex justify-content-center">
+                    <img src="{{ $employer->getLogo() }}" class="img-fluid z-depth-1 rounded-circle"
+                         alt="Responsive image" style="max-height: 95px">
                 </div>
-                <div class="col">
-                    <p class="small">Hello</p>
-                    <span class="card-title"><b>{{ $employer->name }}</b></span>
+                <div class="col-12 col-sm-10 col-lg-4 d-flex flex-column">
+                    <p class="text-muted mb-1 font-weight-light">Hello</p>
+                    <p class="h4 font-weight-normal">{{ $employer->name }}</p>
                 </div>
-                <div class="col">
+                <div class="col-12 col-sm-3 offset-sm-2 col-lg-2 offset-lg-0 d-flex flex-column">
                     @isset($employer->size)
-                        <p class="small">Company size</p>
-                        <span class="card-title"><b>{{ $employer->size }}</b></span>
+                        <p class="text-muted mb-1 font-weight-light" style="word-wrap:normal">Company size</p>
+                        <p class="h4 font-weight-normal">{{ $employer->size }}</p>
                     @endisset
                 </div>
-                <div class="col">
+                <div class="col-12 col-sm-7 col-lg-4 d-flex flex-column">
                     @isset($employer->website)
-                        <p class="small">Website</p>
-                        <span
-                            class="card-title ">
-                                        <a
-                                            target="_blank" href="{{ $employer->website }}">{{ $employer->website }}
-                                        </a>
-                                </span>
+                        <p class="text-muted mb-1 font-weight-light">Website</p>
+                        <p class="h4 font-weight-normal">
+                            <a target="_blank" href="{{ $employer->website }}">{{ $employer->website }}</a>
+                        </p>
                     @endisset
                 </div>
             </div>
         </div>
-        <div class="card">
-            <div class="row card-content">
-                <div class="col s12">
-                    <div class="row">
-                        <div class="col s6">
-                            <span class="card-title left-align">Your offers</span>
-                        </div>
-                        <div class="col s6 right-align">
-                            <a class="waves-effect waves-light btn"
-                               href="{{route('offers.create')}}">{{'Add New Offer'}}</a>
-                        </div>
-                    </div>
-                    <table class="highlight">
-                        <thead>
+        <div class="card mt-5">
+            <div class="card-body">
+                <div class="card-title d-flex justify-content-between align-items-center">
+                    <h4>Your offers</h4>
+                    <a class="btn light-blue darken-2 white-text px-3"
+                       href="{{route('offers.create')}}">{{'Add New Offer'}}
+                    </a>
+                </div>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody class="table-hover">
+                    @foreach( $offers as $offer )
                         <tr>
-                            <th>No.</th>
-                            <th>Title</th>
-                            <th class="right-align">&nbsp;</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach( $offers as $offer )
-                            <tr>
-                                <td>{{ $offer->id }}</td>
-                                <td><a href="/#/details/{{$offer->id}}">{{ $offer->name }}</a></td>
-                                <td class="right-align">
-                                    <a class="waves-effect waves-light btn btn-small" title="Edit this offer"
+                            <td>
+                                <a class="btn btn-link blue-text btn-lg"
+                                   href="/#/details/{{$offer->id}}">{{ $offer->name }}</a>
+                            </td>
+                            <td>
+                                <div class="d-flex justify-content-end">
+                                    <a class="btn btn-default px-3 mr-3" title="Edit this offer"
                                        href="{{ route('offers.edit', $offer) }}">
-                                        <i class="material-icons">edit</i>
+                                        <i class="fas fa-pencil-alt" aria-hidden="true"></i>
                                     </a>
-                                    <a class="waves-effect waves-light btn btn-small red" title="Remove this offer"
-                                       href="#!"
+                                    <a class="btn btn-danger px-3" title="Remove this offer" href="#!"
                                        onclick="event.preventDefault();document.getElementById('delete-offer-{{$offer->id}}').submit();">
-                                        <i class="material-icons">delete</i>
+                                        <i class="fas fa-trash-alt" aria-hidden="true"></i>
                                         <form id="delete-offer-{{$offer->id}}"
                                               action="{{ route('offers.destroy', $offer) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
                                         </form>
                                     </a>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                    {{ $offers->links('components.pagination') }}
-                </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
