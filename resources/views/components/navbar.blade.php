d0e5639cacf4e5f929ac95fdf0e36da1bc89405b
Bartek Olszanowski <baloo@int.pl>
2020-03-31 12:33:17 +0200

    L-45 Aktualizacja navbara

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -17,87 +17,50 @@
         ];
     }
 @endphp
-<nav class="amber" role="navigation">
-    <div class="nav-wrapper container">
-        <a id="logo-container" href="/" class="brand-logo">{{ config('app.name') }}</a>
-        @guest
-            <ul class="right hide-on-med-and-down">
-                @foreach( $guest_links as $link )
-                    <li><a href="{{ $link['route'] }}">{{ $link['name'] }}</a></li>
-                @endforeach
-            </ul>
-            <ul id="nav-mobile" class="sidenav">
-                @foreach( $guest_links as $link )
-                    <li><a href="{{ $link['route'] }}">{{ $link['name'] }}</a></li>
-                @endforeach
-            </ul>
-            <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        @else
-            <ul class="right hide-on-med-and-down">
-                <li>
-                    <a class="dropdown-trigger no-autoinit" href="#"
-                       data-target='dropdown1'>{{ Auth::user()->name }}</a>
-                </li>
-                <ul id="dropdown1" class='dropdown-content'>
-                    @if( auth()->user()->isEmployer() )
-                        @foreach( $employer_links as $link)
-                            <li>
-                                <a class="black-text" href="{{ $link['route'] }}"><i
-                                        class="material-icons">{{ $link['icon'] }}</i>{{ $link['name'] }}</a>
-                            </li>
-                        @endforeach
-                    @endif
-                    @if( auth()->user()->isAdmin() )
-                        @foreach( $admin_links as $link)
-                            <li>
-                                <a class="black-text" href="{{ $link['route'] }}"><i
-                                        class="material-icons">{{ $link['icon'] }}</i>{{ $link['name'] }}</a>
-                            </li>
-                        @endforeach
-                    @endif
-                    <li>
-                        <a class="black-text" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                           document.getElementById('logout-form').submit();">
-                            <i class="material-icons">exit_to_app</i>{{ __('Logout') }}
-                        </a>
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark amber" role="navigation">
 
+    <!-- Navbar brand -->
+    <a id="logo-container" href="/" class="navbar-brand">{{ config('app.name') }}</a>
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavigation"
+            aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="mainNavigation">
+        @guest
+            <ul class="navbar-nav ml-auto">
+                @foreach( $guest_links as $link )
+                    <li class="nav-item"><a class="nav-link" href="{{ $link['route'] }}">{{ $link['name'] }}</a></li>
+                @endforeach
+            </ul>
+        @else
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                       aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
+                    <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                        @if( auth()->user()->isEmployer() )
+                            @foreach( $employer_links as $link)
+                                <a class="dropdown-item" href="{{ $link['route'] }}">{{ $link['name'] }}</a>
+                            @endforeach
+                        @endif
+                        @if( auth()->user()->isAdmin() )
+                            @foreach( $admin_links as $link)
+                                <a class="dropdown-item" href="{{ $link['route'] }}">{{ $link['name'] }}</a>
+                            @endforeach
+                        @endif
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
                         </form>
-                    </li>
-                </ul>
-
-            </ul>
-            <ul id="nav-mobile" class="sidenav">
-                @if( auth()->user()->isEmployer() )
-                    @foreach( $employer_links as $link)
-                        <li>
-                            <a class="black-text" href="{{ $link['route'] }}"><i
-                                    class="material-icons">{{ $link['icon'] }}</i>{{ $link['name'] }}</a>
-                        </li>
-                    @endforeach
-                @endif
-                @if( auth()->user()->isAdmin() )
-                    @foreach( $admin_links as $link)
-                        <li>
-                            <a class="black-text" href="{{ $link['route'] }}"><i
-                                    class="material-icons">{{ $link['icon'] }}</i>{{ $link['name'] }}</a>
-                        </li>
-                    @endforeach
-                @endif
-                <li>
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                        document.getElementById('logout-form-mobile').submit();">
-                        <i class="material-icons">exit_to_app</i>{{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form-mobile" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
+                    </div>
                 </li>
             </ul>
-            <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         @endguest
     </div>
 </nav>
