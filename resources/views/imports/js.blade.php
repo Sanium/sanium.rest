a66aa70ad61ad7389a43c79191c1157547e1a1f4
Bartłomiej Olszanowski <baloo@int.pl>
2020-04-20 20:18:09 +0200

    L-58 Usunięcie hashy z frontu

--- resources/views/imports/js.blade.php
+++ resources/views/imports/js.blade.php
@@ -1,7 +1,7 @@
-<script src="{{ asset('storage/web/runtime-es2015.c9afb3256f2870e161de.js') }}" type="module"></script>
-<script src="{{ asset('storage/web/runtime-es5.c9afb3256f2870e161de.js') }}" nomodule defer></script>
-<script src="{{ asset('storage/web/polyfills-es5.070afc62f0720ac803de.js') }}" nomodule defer></script>
-<script src="{{ asset('storage/web/polyfills-es2015.d975337fea7f8bffabc0.js') }}" type="module"></script>
-<script src="{{ asset('storage/web/scripts.2fd360e0f1643d959e0d.js') }}" defer></script>
-<script src="{{ asset('storage/web/main-es2015.6103b7d2a93587d5c6f8.js') }}" type="module"></script>
-<script src="{{ asset('storage/web/main-es5.6103b7d2a93587d5c6f8.js') }}" nomodule defer></script>
+<script src="{{ asset('storage/web/runtime-es2015.js') }}" type="module"></script>
+<script src="{{ asset('storage/web/runtime-es5.js') }}" nomodule defer></script>
+<script src="{{ asset('storage/web/polyfills-es5.js') }}" nomodule defer></script>
+<script src="{{ asset('storage/web/polyfills-es2015.js') }}" type="module"></script>
+<script src="{{ asset('storage/web/scripts.js') }}" defer></script>
+<script src="{{ asset('storage/web/main-es2015.js') }}" type="module"></script>
+<script src="{{ asset('storage/web/main-es5.js') }}" nomodule defer></script>
