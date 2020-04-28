855a2c6372802e71a6b219c7f4cf4bac4b4749c1
darullef <darullef4@gmail.com>
2020-04-28 22:19:44 +0200

    L-59 Fixy wyglądu widoków weryfikacji

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,35 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col s12 m8 offset-m2 l6 offset-l3 xl6 offset-xl3">
-                <div class="card">
-                    <div class="card-content">
-                        <div class="row">
-                            <span class="card-title">{{ __('Verify Your Email Address') }}</span>
-                            <p>
-                                {{ __('Before proceeding, please check your email for a verification link.') }}
-                            </p>
-                            <p>
-                                {{ __('If you did not receive the email') }}
-                            </p>
-                        </div>
-                        <div class="row center-align">
-                            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                                @csrf
-                                <button type="submit"
-                                        class="col s12 btn waves-effect waves-light">{{ __('click here to request another') }}</button>
-                            </form>
-
-                            <p class="small">
-                            @if ( session('resent'))
-                                    {{ __('A fresh verification link has been sent to your email address.') }}
-                            @endif
-                                &nbsp;
-                            </p>
-                        </div>
-                    </div>
+    <div class="container-fluid d-flex justify-content-center mt-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                    <h5>{{ __('Verify Your Email Address') }}</h5>
+                </div>
+                <div>
+                    <p>
+                        {{ __('Before proceeding, please check your email for a verification link.') }}
+                    </p>
+                    <p>
+                        {{ __('If you did not receive the email') }}
+                    </p>
+                </div>
+                <div>
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        <button type="submit" class="btn light-blue darken-2 text-white btn-block my-4">{{ __('click here to request another') }}</button>
+                    </form>
+                    @if (session('resent'))
+                        <p class="text-muted text-center">{{ __('A fresh verification link has been sent to your email address.') }}</p>
+                    @endif
                 </div>
             </div>
         </div>
