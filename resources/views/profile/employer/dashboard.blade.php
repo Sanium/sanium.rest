a0b9f56fe908281a92787b299955a63a0f14118c
darullef <darullef4@gmail.com>
2020-05-16 19:51:58 +0200

    L-78 Dodanie widoku zgłoszeń na oferty w dashboardzie pracodawcy

--- resources/views/profile/employer/dashboard.blade.php
+++ resources/views/profile/employer/dashboard.blade.php
@@ -33,73 +33,202 @@
             </div>
         </div>
         <div class="card mt-5">
+            <div class="card-title mb-0">
+                <ul class="nav nav-tabs nav-justified md-tabs light-blue m-0 no-bottom-radius z-depth-0" id="myTabJust"
+                    role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="home-tab-just" data-toggle="tab" href="#offers-tab" role="tab"
+                           aria-controls="home-just"
+                           aria-selected="true">{{ __('Your offers') }}</a></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="profile-tab-just" data-toggle="tab" href="#applications-tab" role="tab"
+                           aria-controls="profile-just"
+                           aria-selected="false">{{ __('Applications') }}</a></a>
+                    </li>
+                </ul>
+            </div>
             <div class="card-body">
-                <div class="card-title d-flex justify-content-between align-items-center">
-                    <h4>@lang('Your offers')</h4>
-                    <a class="btn light-blue darken-2 white-text px-3"
-                       href="{{route('offers.create')}}">@lang('Add new offer')
-                    </a>
-                </div>
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">@lang('Title')</th>
-                        <th scope="col">@lang('Will expire on')</th>
-                        <th scope="col"></th>
-                    </tr>
-                    </thead>
-                    <tbody class="table-hover">
-                    @foreach( $offers as $offer )
-                        <tr>
-                            <td>
-                                <a class="btn btn-link blue-text btn-lg"
-                                   href="/#/details/{{$offer->id}}">
-                                    {{ $offer->name }}
-                                </a>
-                            </td>
-                            <td class="" style="vertical-align: middle">
-                                {{ $offer->expires_at }}
-                                @if( $offer->isExpired() )
-                                    <span class="badge badge-danger ml-2">@lang('Expired')</span>
-                                @endif
-                            </td>
-                            <td>
-                                <div class="d-flex justify-content-end">
-                                    @if( $offer->isExpired() )
-                                        <button class="btn btn-warning px-3 m-0 mr-3" title="@lang('Refresh offer')"
-                                            onclick="event.preventDefault();document.getElementById('refresh-offer-{{$offer->id}}').submit();">
-                                            <i class="fas fa-redo-alt" aria-hidden="true"></i>
-                                        </button>
-                                        <form id="refresh-offer-{{$offer->id}}"
-                                              action="{{ route('offers.refresh', $offer) }}" method="POST">
-                                            @csrf
-                                        </form>
-                                    @endif
-                                    <a class="btn btn-default px-3 mr-3" title="@lang('Edit this offer')"
-                                       href="{{ route('offers.edit', $offer) }}">
-                                        <i class="fas fa-pencil-alt" aria-hidden="true"></i>
+                <div class="tab-content p-0">
+                    <div class="tab-pane fade show active" id="offers-tab" role="tabpanel">
+                        <div class="card-title d-flex justify-content-between align-items-center">
+                            <h4>@lang('Your offers')</h4>
+                            <a class="btn light-blue darken-2 white-text px-3"
+                               href="{{route('offers.create')}}">@lang('Add new offer')
+                            </a>
+                        </div>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">@lang('Title')</th>
+                                <th scope="col">@lang('Will expire on')</th>
+                                <th scope="col"></th>
+                            </tr>
+                            </thead>
+                            <tbody class="table-hover">
+                            @foreach( $offers as $offer )
+                                <tr>
+                                    <td>
+                                        <a class="btn btn-link blue-text btn-lg"
+                                           href="/#/details/{{$offer->id}}">
+                                            {{ $offer->name }}
+                                        </a>
+                                    </td>
+                                    <td class="" style="vertical-align: middle">
+                                        {{ $offer->expires_at }}
+                                        @if( $offer->isExpired() )
+                                            <span class="badge badge-danger ml-2">@lang('Expired')</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <div class="d-flex justify-content-end">
+                                            @if( $offer->isExpired() )
+                                                <button class="btn btn-warning px-3 m-0 mr-3"
+                                                        title="@lang('Refresh offer')"
+                                                        onclick="event.preventDefault();document.getElementById('refresh-offer-{{$offer->id}}').submit();">
+                                                    <i class="fas fa-redo-alt" aria-hidden="true"></i>
+                                                </button>
+                                                <form id="refresh-offer-{{$offer->id}}"
+                                                      action="{{ route('offers.refresh', $offer) }}" method="POST">
+                                                    @csrf
+                                                </form>
+                                            @endif
+                                            <a class="btn btn-default px-3 mr-3" title="@lang('Edit this offer')"
+                                               href="{{ route('offers.edit', $offer) }}">
+                                                <i class="fas fa-pencil-alt" aria-hidden="true"></i>
+                                            </a>
+                                            <button class="btn btn-danger px-3 m-0" title="@lang('Remove this offer')"
+                                                    onclick="event.preventDefault();document.getElementById('delete-offer-{{$offer->id}}').submit();">
+                                                <i class="fas fa-trash-alt" aria-hidden="true"></i>
+
+                                            </button>
+                                            <form id="delete-offer-{{$offer->id}}"
+                                                  action="{{ route('offers.destroy', $offer) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div class="d-flex justify-content-center">
+                            {{ $offers->links('components.pagination') }}
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="applications-tab" role="tabpanel">
+                        <div class="accordion md-accordion accordion-blocks" id="accordion" role="tablist"
+                             aria-multiselectable="true">
+                            <div class="card border-light z-depth-0">
+                                <div class="card-header" role="tab">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseUnfiled"
+                                       aria-expanded="true"
+                                       aria-controls="collapseUnfiled">
+                                        <h5 class="mt-1 mb-0">
+                                            <span>Offer's tittle</span>
+                                            <i class="fas fa-angle-down rotate-icon"></i>
+                                        </h5>
                                     </a>
-                                    <button class="btn btn-danger px-3 m-0" title="@lang('Remove this offer')"
-                                       onclick="event.preventDefault();document.getElementById('delete-offer-{{$offer->id}}').submit();">
-                                        <i class="fas fa-trash-alt" aria-hidden="true"></i>
-
-                                    </button>
-                                    <form id="delete-offer-{{$offer->id}}"
-                                          action="{{ route('offers.destroy', $offer) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                    </form>
                                 </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-                <div class="d-flex justify-content-center">
-                    {{ $offers->links('components.pagination') }}
+                                <div id="collapseUnfiled" class="collapse" role="tabpanel"
+                                     aria-labelledby="headingUnfiled"
+                                     data-parent="#accordion">
+                                    <div class="card-body">
+                                        <table class="table">
+                                            <thead>
+                                            <tr>
+                                                <th>{{ __('Full name') }}</th>
+                                                <th>{{ __('Email') }}</th>
+                                                <th>{{ __('CV') }}</th>
+                                                <th>{{ __('Date') }}</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <td>Anki stary</td>
+                                                <td>darullef@gmail.com</td>
+                                                <td>
+                                                    <button class="btn btn-primary btn-sm m-0">
+                                                        <i class="fas fa-download mr-1"></i>
+                                                        {{ __('Download') }}
+                                                    </button>
+                                                </td>
+                                                <td>21:37:2020</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Anki stary</td>
+                                                <td>darullef@gmail.com</td>
+                                                <td>
+                                                    <button class="btn btn-primary btn-sm m-0">
+                                                        <i class="fas fa-download mr-1"></i>
+                                                        {{ __('Download') }}
+                                                    </button>
+                                                </td>
+                                                <td>21:37:2020</td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card border-light z-depth-0">
+                                <div class="card-header" role="tab">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseUnfiled"
+                                       aria-expanded="true"
+                                       aria-controls="collapseUnfiled">
+                                        <h5 class="mt-1 mb-0">
+                                            <span>Offer's tittle</span>
+                                            <i class="fas fa-angle-down rotate-icon"></i>
+                                        </h5>
+                                    </a>
+                                </div>
+                                <div id="collapseUnfiled" class="collapse" role="tabpanel"
+                                     aria-labelledby="headingUnfiled"
+                                     data-parent="#accordion">
+                                    <div class="card-body">
+                                        <table class="table">
+                                            <thead>
+                                            <tr>
+                                                <th>{{ __('Full name') }}</th>
+                                                <th>{{ __('Email') }}</th>
+                                                <th>{{ __('CV') }}</th>
+                                                <th>{{ __('Date') }}</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <td>Anki stary</td>
+                                                <td>darullef@gmail.com</td>
+                                                <td>
+                                                    <button class="btn btn-primary btn-sm m-0">
+                                                        <i class="fas fa-download mr-1"></i>
+                                                        {{ __('Download') }}
+                                                    </button>
+                                                </td>
+                                                <td>21:37:2020</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Anki stary</td>
+                                                <td>darullef@gmail.com</td>
+                                                <td>
+                                                    <button class="btn btn-primary btn-sm m-0">
+                                                        <i class="fas fa-download mr-1"></i>
+                                                        {{ __('Download') }}
+                                                    </button>
+                                                </td>
+                                                <td>21:37:2020</td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </div>
+    </div>
     </div>
 @endsection
