1ef1dbc239db824edd4002dbcfecfad1a54ef3d2
Bartłomiej Olszanowski <baloo@int.pl>
2020-05-18 16:16:53 +0200

    L-71 Poprawki w dashboardach

--- resources/views/profile/client/dashboard.blade.php
+++ resources/views/profile/client/dashboard.blade.php
@@ -9,32 +9,24 @@
 @section('content')
     <div class="container">
         <div class="card mt-5">
-            <div class="card-body">
-                <div class="row d-flex align-items-center">
-                    <div class="col d-flex flex-column">
-                        <p class="text-muted mb-1 font-weight-light">@lang('Hello!')</p>
-                        <p class="h4 font-weight-normal">{{ $client->name }}</p>
-                    </div>
-                    <div class="col d-flex flex-column">
-                        <p class="text-muted mb-1 font-weight-light">Email</p>
-                        <p class="h5 font-weight-normal">
-                            {{ $client->user->email }}
-                        </p>
-                    </div>
-                    <div class="col d-flex flex-column">
-                        <p class="text-muted mb-1 font-weight-light">Links</p>
-                        <p class="h5 font-weight-normal">
-                            <i class="fab fa-github">
-                                <a href="{{ $client->links }}">{{ $client->links }}</a>
-                            </i>
-                        </p>
-                    </div>
+            <div class="card-body row d-flex align-items-center">
+                <div class="col-12 col-sm-7 d-flex flex-column">
+                    <p class="text-muted mb-1 font-weight-light">@lang('Hello!')</p>
+                    <p class="h4 font-weight-normal">{{ $client->name }}</p>
+                </div>
+                <div class="col-12 col-sm-5 d-flex flex-column">
+                    <p class="text-muted mb-1 font-weight-light">Email</p>
+                    <p class="h5 font-weight-normal">
+                        <a href="mailto:{{ $client->user->email }}">{{ $client->user->email }}</a>
+                    </p>
                 </div>
             </div>
         </div>
         <div class="card mt-5">
             <div class="card-body">
-                <h5 class="card-title">{{ __('Your applications') }}</h5>
+                <div class="card-title d-flex justify-content-between align-items-center">
+                    <h4>@lang('Your applications')</h4>
+                </div>
                 <table class="table">
                     <thead>
                     <tr>
